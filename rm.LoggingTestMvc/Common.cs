3579390acbec844b1f6ebcd7744b600ecdb5948c
hippy <hippy2981@gmail.com>
2016-01-27 21:20:47 -0800

    Fix: Fixing log4net configPath logic for MVC app.

---
+++ rm.LoggingTestMvc/Common.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using rm.Logging;
+
+namespace rm.LoggingTestMvc
+{
+    /// <summary>
+    /// Helper class.
+    /// </summary>
+    class Common
+    {
+        public const int count = 1;
+        ILogger log;
+        public Common(ILogger log)
+        {
+            this.log = log;
+        }
+
+        public void Start()
+        {
+            Info("start");
+            Debug();
+            Error();
+            Fatal();
+            Info("end");
+        }
+        private void Debug()
+        {
+            log.DebugFormat("count: {0}", count);
+        }
+        private void Info(string message)
+        {
+            log.Info(message);
+            log.InfoFormat("{0} {1}", "static", message);
+        }
+        private void Error()
+        {
+            try
+            {
+                Throw();
+            }
+            catch (Exception ex)
+            {
+                log.Error("Error message", ex);
+            }
+        }
+        private void Fatal()
+        {
+            try
+            {
+                Throw();
+            }
+            catch (Exception ex)
+            {
+                log.Fatal("Fatal message", ex);
+            }
+        }
+        private void Throw()
+        {
+            foreach (var i in Enumerable.Range(0, count))
+            {
+                throw new Exception("ex: " + i.ToString(),
+                    new ApplicationException("inner: " + i.ToString())
+                    );
+            }
+        }
+    }
+}
---
+++ rm.LoggingTestMvc/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace rm.LoggingTestMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            Log4NetConfigChangeAtRuntimeTest();
+            return View();
+        }
+
+        private static void Log4NetConfigChangeAtRuntimeTest()
+        {
+            var ts = DateTime.UtcNow;
+            while (ts.AddMinutes(1) > DateTime.UtcNow)
+            {
+                new Foo().Start();
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
