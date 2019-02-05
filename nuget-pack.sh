4d7b5e86c96e8ef736cc87cf54bcc580205c980d
hippy <hippy2981@gmail.com>
2019-02-04 21:27:15 -0800

    nuget: Nugetize (pack, push)

---
+++ nuget-pack.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#usage: nuget-pack <version>
+
+if [[ "$1" == "-h" ]]; then print-file-comments "$0"; exit; fi
+
+version="$1"
+if [[ -z "$version" ]]; then
+	echo 1>&2 "fatal: version required"
+	exec print-file-comments "$0"
+	exit 1
+fi
+
+tag="nuget-$version"
+
+dotnet pack rm.Logging/rm.Logging.csproj \
+	-c Release \
+	--include-symbols //p:SymbolPackageFormat=snupkg \
+	-o ../.nupkg/ \
+	//p:PackageVersion="$version" \
+	//p:PackageReleaseNotes="tag: $tag" \
+	&& git tag "$tag" -m "Create nuget tag $tag"
